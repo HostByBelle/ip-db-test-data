c080fbcd0ea4b4877ce9b66565095c165113812a
Belle Aerni <belleaerni@gmail.com>
2024-03-29 11:28:30 -0700

    Apparently I completely missed two parsers

--- scripts/parse-updown.py
+++ scripts/parse-updown.py
@@ -1,5 +1,5 @@
 import argparse
-import ujson
+import json
 import ipaddress
 
 
@@ -10,35 +10,54 @@ def get_range(address):
     )
 
 
+def merge(existing, new):
+    if existing == new:
+        return existing
+    else:
+        keep = True
+        # Loop through all of the existing values, if they match the new one we can merge them
+        for key, value in existing.items():
+            if new[key] != value:
+                keep = False
+
+        if keep:
+            return existing + new
+        else:
+            print(f"Existing: {existing}. New: {new}")
+            return existing
+
+
 def parse(updown_data, json_file, ipver):
-    data_list = []
+    data_list = {}
 
     if ipver == "ipv6":
         ipver = "ip6"
 
     try:
         # Load existing data from the JSON file
-        with open(json_file, "r") as existing_file:
-            data_list = ujson.load(existing_file)
+        with open(json_file, "r", encoding="utf-8") as existing_file:
+            data_list = json.load(existing_file)
     except FileNotFoundError:
         pass  # File doesn't exist yet, ignore and proceed with an empty list
 
     with open(updown_data, "r") as file:
-        updown_nodes = ujson.load(file)
+        updown_nodes = json.load(file)
         for node in updown_nodes:
-            data_list.append(
-                {
-                    "ip_range": get_range(updown_nodes[node][ipver]),
-                    "country_code": updown_nodes[node]["country_code"],
-                    "city": updown_nodes[node]["city"],
-                    "lat": updown_nodes[node]["lat"],
-                    "lng": updown_nodes[node]["lng"],
-                }
-            )
+            properties = {
+                "country_code": updown_nodes[node]["country_code"],
+                "city": updown_nodes[node]["city"],
+                "lat": updown_nodes[node]["lat"],
+                "lng": updown_nodes[node]["lng"],
+            }
+            cidr = get_range(updown_nodes[node][ipver])
+            if cidr in data_list:
+                data_list[cidr] = merge(data_list[cidr], properties)
+            else:
+                data_list[cidr] = properties
 
         # Write the updated data back to the JSON file
         with open(json_file, "w", encoding="utf-8") as json_file:
-            ujson.dump(data_list, json_file, indent=0, ensure_ascii=False)
+            json.dump(data_list, json_file, indent=0, ensure_ascii=False)
 
 
 if __name__ == "__main__":
